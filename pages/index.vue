4c141e2458e1ee130c578482ccd7d76b9ed616c3
RyzenPan <736122700@qq.com>
2019-12-28 17:47:08 +0800

    完成了首页轮播图

--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,50 @@
 <template>
-  <div>首页</div>
+  <div class="container">
+    <!-- 幻灯片 -->
+    <el-carousel :interval="5000" arrow="always">
+      <el-carousel-item v-for="(item, index) in banners" :key="index">
+        <div
+          class="banner-image"
+          :style="`
+                background:url(${$axios.defaults.baseURL + item.url}) center center no-repeat;
+                background-size:contain contain;
+                `"
+        ></div>
+      </el-carousel-item>
+    </el-carousel>
+  </div>
 </template>
 
 <script>
 export default {
-
+  data() {
+    return {
+      // 轮播图数据
+      banners: []
+    }
+  },
+  async mounted(){
+    const {data : res} = await this.$axios({
+      url:'/scenics/banners'
+    })
+    this.banners = res.data
+  }
 }
 </script>
 
-<style>
+<style scoped lang="less">
+.container {
+  min-width: 1000px;
+  margin: 0 auto;
+  position: relative;
 
+  /deep/ .el-carousel__container {
+    height: 700px;
+  }
+
+  .banner-image {
+    width: 100%;
+    height: 100%;
+  }
+}
 </style>
