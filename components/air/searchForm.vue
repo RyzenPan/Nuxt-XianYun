2ee74d1d836e65e81d2d42a4e28e009a50563a94
RyzenPan <736122700@qq.com>
2020-01-01 10:37:18 +0800

    完成了机票

--- components/air/searchForm.vue
+++ components/air/searchForm.vue
@@ -1,224 +1,308 @@
 <template>
-    <div class="search-form">
+  <div class="search-form">
+    <!-- 头部tab切换 -->
+    <el-row type="flex" class="search-tab">
+      <span
+        v-for="(item, index) in tabs"
+        :key="index"
+        @click="handleSearchTab(item, index)"
+        :class="{active: index === currentTab}"
+      >
+        <i :class="item.icon"></i>
+        {{item.name}}
+      </span>
+    </el-row>
 
-        <!-- 头部tab切换 -->
-        <el-row type="flex" class="search-tab">
-            <span v-for="(item, index) in tabs" :key="index"
-            @click="handleSearchTab(item, index)"
-            :class="{active: index === currentTab}">
-                <i :class="item.icon"></i>{{item.name}}
-            </span>
-        </el-row>
-
-        <el-form class="search-form-content" ref="form" label-width="80px">
-            <el-form-item label="出发城市">
-                <!-- fetch-suggestions 返回输入建议的方法 -->
-                <!-- select 点击选中建议项时触发 -->
-                <el-autocomplete
-                :fetch-suggestions="queryDepartSearch"
-                placeholder="请搜索出发城市"
-                @select="handleDepartSelect"
-                class="el-autocomplete"
-                ></el-autocomplete>
-            </el-form-item>
-            <el-form-item label="到达城市">
-                <el-autocomplete
-                :fetch-suggestions="queryDestSearch"
-                placeholder="请搜索到达城市"
-                @select="handleDestSelect"
-                class="el-autocomplete"
-                ></el-autocomplete>
-            </el-form-item>
-            <el-form-item label="出发时间">
-                <!-- change 用户确认选择日期时触发 -->
-                <el-date-picker type="date" 
-                placeholder="请选择日期" 
-                style="width: 100%;"
-                @change="handleDate">
-                </el-date-picker>
-            </el-form-item>
-            <el-form-item label="">
-                <el-button style="width:100%;" 
-                type="primary" 
-                icon="el-icon-search"
-                @click="handleSubmit">
-                    搜索
-                </el-button>
-            </el-form-item>
-            <div class="reverse">
-                <span @click="handleReverse">换</span>
-            </div>
-        </el-form>  
+    <el-form class="search-form-content" ref="form" label-width="80px">
+      <el-form-item label="出发城市">
+        <!-- fetch-suggestions 返回输入建议的方法 -->
+        <!-- select 点击选中建议项时触发 -->
+        <el-autocomplete
+          :fetch-suggestions="queryDepartSearch"
+          placeholder="请搜索出发城市"
+          @select="handleDepartSelect"
+          class="el-autocomplete"
+          v-model="form.departCity"
+          @blur="departCityInput"
+        ></el-autocomplete>
+      </el-form-item>
+      <el-form-item label="到达城市">
+        <el-autocomplete
+          :fetch-suggestions="queryDestSearch"
+          placeholder="请搜索到达城市"
+          @select="handleDestSelect"
+          class="el-autocomplete"
+          v-model="form.destCity"
+          @blur="destCityInput"
+        ></el-autocomplete>
+      </el-form-item>
+      <el-form-item label="出发时间">
+        <!-- change 用户确认选择日期时触发 -->
+        <el-date-picker
+          type="date"
+          placeholder="请选择日期"
+          style="width: 100%;"
+          v-model="dateValue"
+          @change="handleDate"
+          :picker-options="pickerOptions"
+        ></el-date-picker>
+      </el-form-item>
+      <el-form-item label>
+        <el-button style="width:100%;" type="primary" icon="el-icon-search" @click="handleSubmit">搜索</el-button>
+      </el-form-item>
+      <div class="reverse">
+        <span @click="handleReverse">换</span>
       </div>
+    </el-form>
+  </div>
 </template>
 
 <script>
+var moment = require('moment')
 export default {
-    data(){
-        return {
-            tabs: [
-                {icon: "iconfont icondancheng", name: "单程"},
-                {icon: "iconfont iconshuangxiang", name: "往返"}
-            ],
-            currentTab: 0,
+  data() {
+    return {
+      tabs: [
+        { icon: 'iconfont icondancheng', name: '单程' },
+        { icon: 'iconfont iconshuangxiang', name: '往返' }
+      ],
+      currentTab: 0,
+      dateValue: '',
+      form: {
+        departCity: '',
+        departCode: '',
+        destCity: '',
+        destCode: '',
+        departDate: ''
+      },
+      departList: [],
+      destList: [],
+      pickerOptions: {
+        disabledDate(time) {
+          return time.getTime() + 3600 * 1000 * 24 < Date.now()
         }
-    },
-    methods: {
-        // tab切换时触发
-        handleSearchTab(item, index){
-            
-        },
-        
-        // 出发城市输入框获得焦点时触发
-        // value 是选中的值，cb是回调函数，接收要展示的列表
-        queryDepartSearch(value, cb){
-            cb([
-                {value: 1},
-                {value: 2},
-                {value: 3},
-            ]);
-        },
-
-        // 目标城市输入框获得焦点时触发
-        // value 是选中的值，cb是回调函数，接收要展示的列表
-        queryDestSearch(value, cb){
-            cb([
-                {value: 1},
-                {value: 2},
-                {value: 3},
-            ]);
-        },
-       
-        // 出发城市下拉选择时触发
-        handleDepartSelect(item) {
-            
-        },
-
-        // 目标城市下拉选择时触发
-        handleDestSelect(item) {
-            
-        },
-
-        // 确认选择日期时触发
-        handleDate(value){
-           
-        },
-
-        // 触发和目标城市切换时触发
-        handleReverse(){
-            
-        },
-
-        // 提交表单是触发
-        handleSubmit(){
-           
-        }
-    },
-    mounted() {
-       
+      }
     }
+  },
+  methods: {
+    // tab切换时触发
+    handleSearchTab(item, index) {},
+
+    // 出发城市输入失去焦点
+    departCityInput() {
+      if (this.departList[0]) {
+        this.form.departCode = this.departList[0].sort
+        this.form.departCity = this.departList[0].value
+      }
+    },
+    // 到达城市输入失去焦点
+    destCityInput() {
+      if (this.destList[0]) {
+        this.form.destCode = this.destList[0].sort
+        this.form.destCity = this.destList[0].value
+      }
+    },
+    // 封装提示框城市提示
+    getTips(value, cb, list, callback) {
+      if (value) {
+        this.$axios({
+          url: '/airs/city',
+          params: {
+            name: value
+          }
+        }).then(res => {
+          let list = res.data.data.map(item => {
+            item.value = item.name.replace('市', '')
+            return item
+          })
+          cb(list)
+          callback(list)
+        })
+      } else if (!value) {
+        cb([])
+      }
+    },
+
+    // 出发城市输入框获得焦点时触发
+    // value 是选中的值，cb是回调函数，接收要展示的列表
+    queryDepartSearch(value, cb) {
+      this.getTips(value, cb, this.departList, list => {
+        this.departList = list
+      })
+    },
+
+    // 目标城市输入框获得焦点时触发
+    // value 是选中的值，cb是回调函数，接收要展示的列表
+    queryDestSearch(value, cb) {
+      this.getTips(value, cb, this.destList, list => {
+        this.destList = list
+      })
+    },
+
+    // 出发城市下拉选择时触发
+    handleDepartSelect(item) {
+      this.form.departCity = item.value
+      this.form.departCode = item.sort
+    },
+
+    // 目标城市下拉选择时触发
+    handleDestSelect(item) {
+      this.form.destCity = item.value
+      this.form.destCode = item.sort
+    },
+
+    // 确认选择日期时触发
+    handleDate(value) {
+      this.form.departDate = moment(value).format('YYYY-MM-DD')
+    },
+
+    // 触发和目标城市切换时触发
+    handleReverse() {
+      let { departCity, departCode, destCity, destCode } = this.form
+      this.form.departCity = destCity
+      this.form.departCode = destCode
+      this.form.destCity = departCity
+      this.form.destCode = departCode
+    },
+
+    // 提交表单是触发
+    handleSubmit() {
+      console.log(this.form)
+      const rules = {
+        depart: {
+          value: this.form.departCity,
+          message: '请选择出发城市'
+        },
+        dest: {
+          value: this.form.destCity,
+          message: '请选择到达城市'
+        },
+        departDate: {
+          value: this.form.departDate,
+          message: '请选择出发时间'
+        }
+      }
+      let valid = true
+      Object.keys(rules).forEach(v => {
+        const item = rules[v]
+        if (!item.value) {
+          valid = false
+          this.$message.info(item.message)
+        }
+      })
+      if (!valid) return
+
+      this.$router.push({
+        path: '/air/flights',
+        query: this.form
+      })
+    }
+  },
+  mounted() {}
 }
 </script>
 
 <style scoped lang="less">
-.search-form{
-    border:1px #ddd solid;
-    border-top:none;
-    width:360px;
-    height:350px;
-    box-sizing: border-box;
+.search-form {
+  border: 1px #ddd solid;
+  border-top: none;
+  width: 360px;
+  height: 350px;
+  box-sizing: border-box;
 }
 
-.search-tab{
-  span{
+.search-tab {
+  span {
     display: block;
-    flex:1;
+    flex: 1;
     text-align: center;
-    height:48px;
+    height: 48px;
     line-height: 42px;
     box-sizing: border-box;
-    border-top:3px #eee solid;
-    background:#eee;
+    border-top: 3px #eee solid;
+    background: #eee;
   }
 
-  .active{
+  .active {
     border-top-color: orange;
-    background:#fff;
+    background: #fff;
   }
 
-  i{
-    margin-right:5px;
+  i {
+    margin-right: 5px;
     font-size: 18px;
 
-    &:first-child{
-      font-size:16px;
+    &:first-child {
+      font-size: 16px;
     }
   }
 }
 
-.search-form-content{
-  padding:15px 50px 15px 15px;
+.search-form-content {
+  padding: 15px 50px 15px 15px;
   position: relative;
 
-  .el-autocomplete{
+  .el-autocomplete {
     width: 100%;
   }
 }
 
-.reverse{
-  position:absolute;
+.reverse {
+  position: absolute;
   top: 35px;
-  right:15px;
+  right: 15px;
 
-  &:after,&:before{
-      display: block;
-      content: "";
-      position: absolute;
-      left:-35px;
-      width:25px;
-      height:1px;
-      background:#ccc;
+  &:after,
+  &:before {
+    display: block;
+    content: '';
+    position: absolute;
+    left: -35px;
+    width: 25px;
+    height: 1px;
+    background: #ccc;
   }
 
-  &:after{
-      top:0;
-    }
+  &:after {
+    top: 0;
+  }
 
-    &:before{
-      top:60px;
-    }
+  &:before {
+    top: 60px;
+  }
 
-  span{
+  span {
     display: block;
-    position:absolute;
+    position: absolute;
     top: 20px;
-    right:0;
-    font-size:12px;
+    right: 0;
+    font-size: 12px;
     background: #999;
-    color:#fff;
-    width:20px;
-    height:20px;
+    color: #fff;
+    width: 20px;
+    height: 20px;
     line-height: 18px;
     text-align: center;
     border-radius: 2px;
     cursor: pointer;
 
-    &:after,&:before{
+    &:after,
+    &:before {
       display: block;
-      content: "";
+      content: '';
       position: absolute;
-      left:10px;
-      width:1px;
-      height:20px;
-      background:#ccc;
+      left: 10px;
+      width: 1px;
+      height: 20px;
+      background: #ccc;
     }
 
-    &:after{
-      top:-20px;
+    &:after {
+      top: -20px;
     }
 
-    &:before{
-      top:20px;
+    &:before {
+      top: 20px;
     }
   }
 }
--- store/user.js
+++ store/user.js
@@ -1,5 +1,5 @@
 // 保存user下的数据
-export const state = () => {
+export const state = {
     // name: "某jack"
 
     // 登录后的数据，包含token和user对象
