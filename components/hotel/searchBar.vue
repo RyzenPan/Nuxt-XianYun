cda8976f32cdc7b073cdf89641945357403c3d13
RyzenPan <736122700@qq.com>
2020-01-02 20:23:27 +0800

    初步完成酒店头部样式

--- components/hotel/searchBar.vue
+++ components/hotel/searchBar.vue
@@ -1,20 +1,72 @@
 <template>
   <div>
     <!-- 筛选入住时间 -->
-		<el-form class="search-form-content" ref="form">
-    <el-form-item>
-      <!-- fetch-suggestions 返回输入建议的方法 -->
-      <!-- select 点击选中建议项时触发 -->
-      <el-autocomplete
-        :fetch-suggestions="queryDepartSearch"
-        placeholder="请搜索出发城市"
-        @select="handleDepartSelect"
-        class="el-autocomplete"
-        v-model="form.departCity"
-        @blur="departCityInput"
-      ></el-autocomplete>
-    </el-form-item>
-		</el-form>
+    <el-form class="search-form-content" ref="form">
+      <el-row>
+        <el-col :span="6">
+          <el-form-item>
+            <!-- fetch-suggestions 返回输入建议的方法 -->
+            <!-- select 点击选中建议项时触发 -->
+            <el-autocomplete
+              :fetch-suggestions="queryDepartSearch"
+              placeholder="目的地"
+              @select="handleDepartSelect"
+              class="el-autocomplete"
+              v-model="form.destList"
+              @blur="departCityInput"
+            ></el-autocomplete>
+          </el-form-item>
+        </el-col>
+        <!-- 入住日期选择 -->
+        <el-col :span="9">
+          <el-form-item>
+            <el-date-picker
+              v-model="value1"
+              type="daterange"
+              range-separator="至"
+              start-placeholder="开始日期"
+              end-placeholder="结束日期"
+            ></el-date-picker>
+          </el-form-item>
+        </el-col>
+        <!-- 入住人选择 -->
+        <el-col :span="6">
+          <el-form-item>
+            <el-popover placement="bottom" width="300" trigger="click">
+              <span style="padding-right:58px;">每间:</span>
+              <el-select v-model="value" placeholder="请选择" size="mini" style="width:100px;">
+                <el-option
+                  v-for="item in options"
+                  :key="item.value"
+                  :label="item.label"
+                  :value="item.value"
+                ></el-option>
+              </el-select>
+              <el-select v-model="value" placeholder="请选择" size="mini" style="width:100px;">
+                <el-option
+                  v-for="item in options"
+                  :key="item.value"
+                  :label="item.label"
+                  :value="item.value"
+                ></el-option>
+              </el-select>
+              <el-input
+                placeholder="人数未定"
+                suffix-icon="el-icon-user"
+                v-model="input1"
+                readonly
+                slot="reference"
+              ></el-input>
+              <el-button type="primary" size="mini" style="margin: 15px 0 0 245px;">确定</el-button>
+            </el-popover>
+          </el-form-item>
+        </el-col>
+        <!-- 按钮 -->
+        <el-col :span="2">
+          <el-button type="primary">主要按钮</el-button>
+        </el-col>
+      </el-row>
+    </el-form>
   </div>
 </template>
 
@@ -23,21 +75,74 @@ export default {
   data() {
     return {
       form: {
-        departCity: '南京'
-      }
+        destList: '南京'
+      },
+      value1: '',
+      destList: [],
+      input1: '',
+      options: [
+        {
+          value: '选项1',
+          label: '黄金糕'
+        },
+        {
+          value: '选项2',
+          label: '双皮奶'
+        },
+        {
+          value: '选项3',
+          label: '蚵仔煎'
+        },
+        {
+          value: '选项4',
+          label: '龙须面'
+        },
+        {
+          value: '选项5',
+          label: '北京烤鸭'
+        }
+      ],
+      value: ''
     }
   },
   methods: {
+    // 封装提示框城市提示
+    getTips(value, cb, list, callback) {
+      if (value) {
+        this.$axios({
+          url: '/airs/city',
+          params: {
+            name: value
+          }
+        }).then(res => {
+          let list = res.data.data.map(item => {
+            item.value = item.name.replace('市', '')
+            return item
+          })
+          cb(list)
+          callback(list)
+        })
+      } else if (!value) {
+        cb([])
+      }
+    },
     // 出发城市下拉选择时触发
     handleDepartSelect(item) {},
     // 出发城市输入失去焦点
     departCityInput() {},
     // 出发城市输入框获得焦点时触发
     // value 是选中的值，cb是回调函数，接收要展示的列表
-    queryDepartSearch(value, cb) {}
+    queryDepartSearch(value, cb) {
+      this.getTips(value, cb, this.destList, list => {
+        this.destList = list
+      })
+    }
   }
 }
 </script>
 
-<style>
+<style lang="less" scoped>
+.el-input {
+  width: 210px;;
+}
 </style>
