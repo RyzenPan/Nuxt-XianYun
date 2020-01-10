5c9f6a33a7d8d06413bbe4b22776061508dc2050
RyzenPan <736122700@qq.com>
2020-01-10 17:19:38 +0800

    个人主页提示

---
+++ pages/user/personal.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="personal">
+      <div class="message">
+          个人主页, 暂未开通
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+ <style lang="less" scoped>
+ .message {
+     height: 200px;
+     text-align: center;
+     line-height: 200px;
+     color: #999;
+ }
+ </style>
